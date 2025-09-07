ba64f08e236e8091a9fe33f372b939f28c7bdb29
Pierros Papadeas <pierros@papadeas.gr>
2025-09-08 00:00:24 +0300

    feat(backend): Use environment variable for CORS origin

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // --- Middleware Setup ---
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN, // Use an environment variable for the URL
   credentials: true,
 }));
 
