e815cb8a0c421af3057cdd8169c97703dc4c63ec
Pierros Papadeas <pierros@papadeas.gr>
2025-09-09 15:08:37 +0300

    feat(backend): Add is_reviewed column to reports table correctly

--- backend/migrations/1757419551539_add-reviewed-to-reports.js
+++ backend/migrations/1757419551539_add-reviewed-to-reports.js
@@ -1,18 +1,15 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
-export const shorthands = undefined;
+exports.shorthands = undefined;
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const up = (pgm) => {};
+exports.up = (pgm) => {
+  pgm.addColumns('reports', {
+    is_reviewed: {
+      type: 'boolean',
+      notNull: true,
+      default: false,
+    },
+  });
+};
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const down = (pgm) => {};
+exports.down = (pgm) => {
+  pgm.dropColumns('reports', ['is_reviewed']);
+};
